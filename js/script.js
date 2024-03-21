00c3a48a627f451ff6ce8e73b2c8a2e7869059ee
Axlre <164325906+Axlre@users.noreply.github.com>
2024-03-21 04:32:34 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,8 +2,25 @@ const header = document.querySelector('header');
 
 window.addEventListener('scroll', () =>{
     header.classList.toggle('sticky', this.window.scrollY)
-})
+});
 
+//open menu 
+
+let menu = document.querySelector('menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+//Solucion Window
+
+window.onscroll=() =>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+
+}
 
 
 
