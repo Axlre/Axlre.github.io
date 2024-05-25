8ed8bad1df4dbab4a2ebd56add6f918d8170ad25
Axlre <164325906+Axlre@users.noreply.github.com>
2024-05-24 23:34:40 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector('header');
 
 window.addEventListener('scroll', () =>{
-    header.classList.toggle('sticky', this.window.scrollY > 80);
+    header.classList.toggle('sticky', this.window.scrollY > 5);
 });
 
 //open menu 
